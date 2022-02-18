6909aa57e4ea138832509aeb16d19891c313167a
Patrick Cuba <cubap@slu.edu>
2022-02-18 10:24:28 -0600

    new token refresher (#14)
    * new token refresher
    
    * comments
    
    * I love meth

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 require('dotenv').config()
+require('./tokens')
 
 var indexRouter = require('./routes/index')
 var queryRouter = require('./routes/query')
---
+++ tokens.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+// https://stackoverflow.com/a/69058154/1413302
+const isTokenExpired = (token) => (Date.now() >= JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString()).exp * 1000)
+
+/**
+ * Use the privately stored refresh token to generate a new access token for
+ * your RERUM-connected application. There is no way to authenticate this 
+ * process, so protect your refresh token and replace it if it is exposed. 
+ */
+function generateNewAccessToken() {
+    const payload = {
+        method: 'POST',
+        body: JSON.stringify({ refresh_token: process.env.refresh_token })
+    }
+    fetch(process.env.RERUM_ACCESS_TOKEN_URL, payload)
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            return Promise.reject(response)
+        })
+        .then(tokenObject => process.env.access_token = tokenObject.access_token)
+        .catch(err => console.error("Token not updated: ", err))
+    console.warn("Access Token expired. Consider updating your .env files")
+}
+
+/**
+ * This will conduct a simple check against the expiry date in your token.
+ * This does not validate your access token, so you may still be rejected by 
+ * your RERUM instance as unauthorized.
+ */
+if (isTokenExpired(process.env.access_token)) { generateNewAccessToken() }
