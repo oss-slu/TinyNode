772982f9c66316e5e2c9db1ac76f42d592d74f31
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-03-23 11:13:21 -0500

    Index `app` routing (#29)
    * add index routing for app
    
    * send index page
    
    ---------
    
    Co-authored-by: cubap <cubap@slu.edu>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+
+const root = path.join(__dirname,"../public")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('index.html', {root} )
 });
 
 module.exports = router;
