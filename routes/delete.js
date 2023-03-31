3feee5e03a144ecc7a019cd2b5a718a0efb15087
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-03-31 15:52:15 -0500

    Prepare to deploy and test on vlcdhp02 (#32)

--- routes/delete.js
+++ routes/delete.js
@@ -2,22 +2,44 @@ const express = require('express')
 const router = express.Router()
 const got = require('got')
 
-/* DELETE a delete to the thing. */
-router.delete('/:id', async (req, res, next) => {
+/* Legacy delete pattern w/body */
 
+/* DELETE a delete to the thing. */
+router.delete('/', async (req, res, next) => {
   try {
-    const deleteBody = {'@id':`${process.env.RERUM_ID_PATTERN}${req.params.id}`}
+    const deleteBody = req.body ?? {}
 
     const deleteOptions = {
       json: deleteBody,
       headers: {
         'user-agent': 'Tiny-Node',
-        'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+        'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`,
+        'Content-Type' : "application/json"
       }
     }
     console.log(deleteBody)
-    console.log(JSON.stringify(deleteBody))
     const deleteURL = `${process.env.RERUM_API_ADDR}delete`
+    const result = await got.delete(deleteURL, deleteOptions).text()
+    res.status(204)
+    res.send(result)
+  }
+  catch (err) {
+    console.log(err)
+    res.status(500).send("Caught Error:" + err)
+  }
+})
+
+/* DELETE a delete to the thing. */
+router.delete('/:id', async (req, res, next) => {
+  try {
+  
+    const deleteURL = `${process.env.RERUM_API_ADDR}delete/${req.params.id}`
+    const deleteOptions = {
+      headers: {
+        'user-agent': 'Tiny-Node',
+        'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`,
+      }
+    }
     const result = await got.delete(deleteURL, deleteOptions).text()
     res.status(204)
     res.send(result)
